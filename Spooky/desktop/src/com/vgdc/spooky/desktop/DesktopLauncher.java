342741e6d70695dc7f7dc29a669626a9e6649847
Violet Moore <Pyroan@users.noreply.github.com>
2016-10-20 09:21:06 -0400

    Created the project
    +all the initial files

---
+++ Spooky/desktop/src/com/vgdc/spooky/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.vgdc.spooky.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.vgdc.spooky.Spooky;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Spooky(), config);
+	}
+}
