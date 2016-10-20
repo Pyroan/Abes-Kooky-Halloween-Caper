c36ea97a947fcaae7c416a99b2b2f8c764d92455
Derek Belsky <derekbelsky102@gmail.com>
2016-10-20 17:48:55 -0400

    Maybe I'm dumm who knows Jury is still out
    +Assets stuff.
    *Constants
    *Desktop Launcher

--- Spooky/desktop/src/com/vgdc/spooky/desktop/DesktopLauncher.java
+++ Spooky/desktop/src/com/vgdc/spooky/desktop/DesktopLauncher.java
@@ -2,10 +2,24 @@ package com.vgdc.spooky.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker.Settings;
 import com.vgdc.spooky.Spooky;
 
 public class DesktopLauncher {
+	private static boolean rebuildAtlas  = true;
+	private static boolean drawDebugOutline = false;
 	public static void main (String[] arg) {
+		if(rebuildAtlas)
+		{
+			Settings settings = new Settings();
+			settings.maxWidth = 1024;
+			settings.maxHeight = 1024;
+			settings.duplicatePadding = false;
+			settings.debug = drawDebugOutline;
+			TexturePacker.process(settings, "../core/assets", "../core/assets", "tiles");
+		}
+		
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = 1280;
 		config.height = 720;
