3063bfe743df2c1ab139f4b23aa4ffbba9c65e35
Pyroan <Pyroan@users.noreply.github.com>
2016-10-24 07:34:03 -0400

    Still mostly refactoring, haven't changed the algorithm yet.
    *Assets: Deleted an old TODO
    *Level: Got rid of some dirty (and erroneous) hardcoding
    *WorldController: Refactored to work with MapGenerator's new Structure
    *CameraHelper: Adjusts position when debugging map to display the entire map
    *Constants: Updated 2 comments, now adjusts viewport size based on whether we're debugging the map.
    NOTE: DEBUGGING_MAP MUST BE SET TO FALSE IF YOU WANT THE GAME TO WORK AS NORMAL
    *MapGenerator: Added some comments, now comes with extra instance variables free of charge, move parameters from generate() to the constructor, increased spawn rate of trees in order to create more closed off areas for me to fix, now uses setObject(int) instead of Pixmap.setColor so I don't have to retype that all the time, renamed i and j to be a bit more clear, added an empty method to fix the pathing.

--- Spooky/core/src/com/vgdc/utils/Constants.java
+++ Spooky/core/src/com/vgdc/utils/Constants.java
@@ -6,17 +6,17 @@ package com.vgdc.utils;
  */
 public class Constants {
 
-	// Are we debugging stuff?
-	public static final boolean DEBUGGING_MAP = false;
-	
-	// The default window size.
-	public static final float VIEWPORT_WIDTH = DEBUGGING_MAP ? 16 : 6;
-	public static final float VIEWPORT_HEIGHT = DEBUGGING_MAP ? 16 : 6;
+	// Are we debugging map generation?
+	public static final boolean DEBUGGING_MAP = true;
 
 	// Our default map size (in tiles)
-	// 	not sure but going to assume MUST BE POWER OF 2!!!!!
 	public static final int MAP_WIDTH = 32;
 	public static final int MAP_HEIGHT = 32;
+	
+	// The default window size.
+	// I don't fully understand how this works.
+	public static final float VIEWPORT_WIDTH = DEBUGGING_MAP ? MAP_WIDTH : 6;
+	public static final float VIEWPORT_HEIGHT = DEBUGGING_MAP ? MAP_HEIGHT : 6;
 
 	// Reference to the Texture Atlas
 	public static final String TEXTURE_ATLAS_OBJECTS = "tiles.atlas";
