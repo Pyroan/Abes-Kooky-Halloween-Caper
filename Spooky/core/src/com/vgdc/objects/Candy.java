868b738006119fca1868f35bf9af82aee3702b71
Pyroan <Pyroan@users.noreply.github.com>
2016-11-19 20:32:54 -0500

    Updated more Assets. Mini-commits are fun.
    *Abe assets: bigger.
    *snow: different. Bigger.
    +Candy.

--- Spooky/core/src/com/vgdc/objects/Candy.java
+++ Spooky/core/src/com/vgdc/objects/Candy.java
@@ -4,6 +4,8 @@ import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Pixmap.Format;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.vgdc.spooky.Assets;
 
 /**
  * Our Player: A small pink rectangle..
@@ -12,7 +14,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
  */
 public class Candy extends AbstractGameObject
 {
-	private Texture tex;
+	private TextureRegion reg;
 
 	public boolean collected;
 
@@ -22,7 +24,8 @@ public class Candy extends AbstractGameObject
 		dimension.set(1, 1);
 		pixmap.setColor(1f,0f,.75f,1f);
 		pixmap.fillRectangle(30, 50, pixmap.getWidth() - 60, pixmap.getHeight()-100);
-		tex = new Texture(pixmap);
+		reg = Assets.instance.candy.candy;
+		
 		collected = false;
 		bounds.set(0,0, dimension.x, dimension.y);
 		origin.set(dimension.x/2, dimension.y/2);
@@ -31,18 +34,18 @@ public class Candy extends AbstractGameObject
 	@Override
 	public void render(SpriteBatch batch) {
 		if (collected) return;
-		batch.draw(tex, position.x, position.y, origin.x,
-				origin.y, dimension.x, dimension.y, scale.x,
-				scale.y, rotation, 0, 0, tex.getWidth(),
-				tex.getHeight(), false, false);
+		batch.draw(reg.getTexture(), position.x, position.y,
+				origin.x, origin.y, dimension.x, dimension.y,
+				scale.x, scale.y, rotation, reg.getRegionX(), reg.getRegionY(),
+				reg.getRegionWidth(), reg.getRegionHeight(), false, false);
 	}
 	@Override
 	public float getWidth() {
-		return tex.getWidth();
+		return reg.getRegionWidth();
 	}
 	@Override
 	public float getHeight() {
-		return tex.getHeight();
+		return reg.getRegionHeight();
 	}
 
 }
