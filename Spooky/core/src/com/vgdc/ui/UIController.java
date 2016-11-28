73c2632ffcc387ae3b48e63d472a6be061c45e48
Violet Moore <Pyroan@users.noreply.github.com>
2016-11-28 08:11:59 -0500

    Got rid of House (How was that still there?),  Refactored Timer.
    *UIController: Not 100% Sure what happened here, looks like Line Endings may hae changed or something.
    *GameTimer: Changed a couple comments and got rid of an unnecessary if statement. Plus I got rid of that unnecessary render() method and import, meaning this is now a pretty much independent class.

--- Spooky/core/src/com/vgdc/ui/UIController.java
+++ Spooky/core/src/com/vgdc/ui/UIController.java
@@ -17,39 +17,39 @@ import com.vgdc.utils.GameTimer;
 public class UIController {
 	// I'm very familiar with the feeling of having no earthly
 	// idea what I'm doing.
-	
+
 	private Minimap minimap; // -shrug-
 	private GameTimer timer; // -another, slightly bigger shrug
 	private CandyCounter counter; // -a third, even bigger shrug-
 	// Look, it's shrugs all the way down, ok?
-	
+
 	FreeTypeFontGenerator ftGen = new FreeTypeFontGenerator(Gdx.files.internal("Gamer.ttf"));
 	FreeTypeFontParameter param = new FreeTypeFontParameter();
 	BitmapFont font;
-	
+
 	public UIController()
 	{
 		// Set up some font stuff.
 		param.size = 24;
 		font = ftGen.generateFont(param);
 		// Set up the timer.
-		timer = new GameTimer();
+		timer = new GameTimer(7195);
 //		minimap = new Minimap();
 		counter = new CandyCounter();
 	}
-	
+
 	public void update(float deltaTime)
 	{
 		timer.update(deltaTime);
 //		minimap.update(deltaTime);
 	}
-	
+
 	public void render(SpriteBatch batch)
 	{
 		drawTime(batch);
 //		minimap.render(batch);
 	}
-	
+
 	/**
 	 * Draws the GameTimer's time, in a box.
 	 * @param batch
@@ -57,10 +57,10 @@ public class UIController {
 	public void drawTime(SpriteBatch batch)
 	{
 		String time = timer.getTime();
-		
+
 		// Draw the text
 		font.setColor(Color.RED);
 		font.draw(batch, time, 10,
-				Constants.VIEWPORT_GUI_HEIGHT - 10);	
+				Constants.VIEWPORT_GUI_HEIGHT - 10);
 	}
 }
--- Spooky/core/src/com/vgdc/utils/GameTimer.java
+++ Spooky/core/src/com/vgdc/utils/GameTimer.java
@@ -1,7 +1,5 @@
 package com.vgdc.utils;
 
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-
 /**
  * Keeps track of time in both a raw format and a more
  * user-friendly string format.
@@ -13,8 +11,8 @@ public class GameTimer {
 	private float rawTime;
 	// String representation of time, in form HH:MM:SS
 	private String time = "";
-	
-	
+
+
 	/**
 	 * Creates a new Timer starting at 0 seconds.
 	 */
@@ -22,7 +20,7 @@ public class GameTimer {
 	{
 		this(0);
 	}
-	
+
 	/**
 	 * If you want to initialize this with a specific time for some reason.
 	 * @param startTime Time (in seconds) to start the timer from.
@@ -32,26 +30,24 @@ public class GameTimer {
 		rawTime = startTime;
 		translateTime();
 	}
-	
+
 	/**
-	 * Turns raw time into actual real time that we can 
+	 * Turns raw time into actual real time that we can
 	 * look at and not throw up.
 	 */
 	private void translateTime()
 	{
+		// Turns seconds into minutes and seconds.
 		int minutes = (int)rawTime / 60;
 		int seconds = (int)rawTime % 60;
-		int hours = 0;	// If anyone winds up playing the game
-						// long enough to see hours, ohhh boy.
+		int hours = 0; // I'll be amazed if this is needed.
+
 		// Turns minutes bigger than 59 into hours.
-		if (minutes >= 60)
-		{
-			hours = minutes / 60;
-			minutes = minutes % 60;
-		}
-		
-		// Calculations done. Now I gotta make it pretty.
-		
+		hours = minutes / 60;
+		minutes = minutes % 60;
+
+		// Calculations done. Time to convert to a pretty string.
+
 		// Only bother with hours if they've actually passed.
 		if (hours > 0) time = hours + ":";
 		else time = " ";
@@ -61,7 +57,7 @@ public class GameTimer {
 		if (seconds < 10) time+="0";
 		time+= seconds + "";
 	}
-	
+
 	/**
 	 * Adds the time since last frame to the current time.
 	 * @param deltaTime
@@ -71,16 +67,7 @@ public class GameTimer {
 		rawTime+=deltaTime;
 		translateTime();
 	}
-	
-	public void render(SpriteBatch batch)
-	{
-		// JOKE'S ON YOU NO RENDERING HERE YET.
-		
-		// Uncomment this to spam the console with
-		// debug messages.
-//		System.out.println("Timer: " + time);
-	}
-	
+
 	/**
 	 * @return String representation of time, in form (HH:)MM:SS
 	 */
@@ -88,7 +75,7 @@ public class GameTimer {
 	{
 		return time;
 	}
-	
+
 	/**
 	 * @return Raw time in seconds (float).
 	 */
@@ -96,5 +83,5 @@ public class GameTimer {
 	{
 		return rawTime;
 	}
-	
+
 }
