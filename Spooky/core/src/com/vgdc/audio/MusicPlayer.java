60a48cdf987f68d568e7dd862848637bf4c5d936
Unknown <nathanielmanning16@gmail.com>
2016-12-22 00:15:55 -0500

    Added more snores
    added more snore sounds, the snore used in game is chosen using Math.random().

--- Spooky/core/src/com/vgdc/audio/MusicPlayer.java
+++ Spooky/core/src/com/vgdc/audio/MusicPlayer.java
@@ -14,6 +14,9 @@ public class MusicPlayer
 {
 	private Music backgroundSong;
 	private Music nathanielSnoring;
+	private Music snore1;
+	private Music snore2;
+	private Music snore3;
 	private Music wind;
 
 	/**
@@ -25,7 +28,16 @@ public class MusicPlayer
 				Gdx.audio.newMusic(Gdx.files.internal("Snow in October.mp3"));
 		nathanielSnoring =
 				Gdx.audio.newMusic(Gdx.files.internal("Nathaniel Snoring.mp3"));
+		snore1 = 
+				Gdx.audio.newMusic(Gdx.files.internal("NathanielSnoring1.mp3"));
+		snore2 = 
+				Gdx.audio.newMusic(Gdx.files.internal("NathanielSnoring1.mp3"));
+		snore3 = 
+				Gdx.audio.newMusic(Gdx.files.internal("NathanielSnoring1.mp3"));
 		wind = Gdx.audio.newMusic(Gdx.files.internal("wind.mp3"));
+		wind.setVolume(0.2f);
+		backgroundSong.setVolume(.5f);
+		backgroundSong.play();
 	}
 
 	/**
@@ -33,17 +45,23 @@ public class MusicPlayer
 	 * @param deltaTime
 	 */
 	public void update(float deltaTime) {
-		backgroundSong.setVolume(.5f);
-		backgroundSong.play();
-		if (Math.random() > .98)
+		
+		if (Math.random() > .99)
 		{
-			nathanielSnoring.play();
+			double snoreChoice = Math.random();
+			if(snoreChoice <= 0.33)
+			{
+				snore1.play();
+			}
+			if(snoreChoice > 0.33 && snoreChoice <= 0.66)
+			{
+				snore2.play();
+			}
+			if(snoreChoice > 0.66)
+			{
+				snore3.play();
+			}
 		}
-		if (nathanielSnoring.getPosition() >= 7.0f)
-		{
-			nathanielSnoring.stop();
-		}
-		wind.setVolume(0.2f);
 		wind.play();
 	}
 }
